b773c4cd995441750311aea0fecf09e25707bcd2
Anderson Osagie <osagie@gmail.com>
2012-06-23 16:17:29 -0400

    Fleshing out the data model

--- static/js/signboardctrls.js
+++ static/js/signboardctrls.js
@@ -1,13 +1,25 @@
-function BoardCtrl($scope) {
+function BoardCtrl($scope, $http, $log) {
+
+    $scope.signboard = {};
+
+    $scope.loadSignboard = function() {
+        //Loading from the filesystem from now. Using an actual database later.
+        $http.get("static/js/signboard.json").success(function(data, status) {
+            $scope.signboard = data;
+            $log.log("Successfully retrieved signboard data");
+        });
+    };
+
+    $scope.loadSignboard();
+
 }
 
 function ColumnCtrl($scope) {
 }
 
-function TaskCtrl($scope) {
+function CardCtrl($scope) {
 }
 
 angular.module('Signboard', [], function ($interpolateProvider) {
-    $interpolateProvider.startSymbol("{*");
-    $interpolateProvider.endSymbol("*}");
+    $interpolateProvider.startSymbol("{*").endSymbol("*}"); //Default symbols conflict with Jinja2
 });
