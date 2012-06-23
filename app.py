162465f0d19a98d35492c140eeb3b9c0e4c69dc7
Anderson Osagie <osagie@gmail.com>
2012-06-23 14:23:27 -0400

    More wiring

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("signboard.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
