4c728be5d45c24d5a542e5d51241018eb712e8b6
Anderson Osagie <osagie@gmail.com>
2012-06-25 02:10:34 -0400

    Made jQueryUI's event's update AngularJS's data models

--- static/js/signboardComponents.js
+++ static/js/signboardComponents.js
@@ -4,11 +4,43 @@ angular.module("SignboardComponents", [])
             restrict: "E",
             templateUrl: "columnTemplate",
             replace: true,
-            link: function(scope, element, attrs) {
+            link: function(scope, iElement, iAttrs) {
 
-                $(element).find(".cardList").disableSelection().sortable({
-                    connectWith: ".cardList"
+                $(iElement).find(".cardList").disableSelection().sortable({
+                    connectWith: ".cardList",
+                    receive: function(event, ui) { //Handles when the column receives a card from another column
+                        var cardId = ui.item.children().data("card-id");
+
+                        scope.$apply(function() {
+                            scope.addCard(cardId);
+                        });
+                    },
+                    remove: function(event, ui) { //Handles when the column loses a card to another column
+                        var cardId = ui.item.children().data("card-id");
+
+                        scope.$apply(function() {
+                            scope.removeCard(cardId);
+                        });
+                    }
                 });
+
+            },
+            controller: function($scope) {
+
+                $scope.addCard = function(cardId) {
+                    if (!$scope.column.cardIds) {
+                        $scope.column.cardIds = [];
+                    }
+                    $scope.column.cardIds.push(cardId);
+                };
+
+                $scope.removeCard = function(cardId) {
+                    for (var i = 0; i < $scope.column.cardIds.length; i++) {
+                        if ($scope.column.cardIds[i] === cardId) {
+                            $scope.column.cardIds.splice(i, 1);
+                        }
+                    }
+                };
 
             }
         };
